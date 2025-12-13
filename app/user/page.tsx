b9b4a77cceaa78162495589d076491dd7d636b8e
muhammadsadoon <muhammadsadoonsohail786@gmail.com>
2025-12-13 20:04:56 +0500

    Set document title on component mount

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+import {useEffect} from "react"
 
 const page = () => {
+  useEffect(()=>{
+    document.title("user profile");
+  },[]);
   return (
     <div>
-      
+      <h1>user profile</h1>
     </div>
   )
 }
 
-export default page
+export default page;
