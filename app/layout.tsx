c133aad6ba53481d1d48f72970685d592fb296f5
muhammadsadoon <muhammadsadoonsohail786@gmail.com>
2025-12-19 21:50:32 +0500

    Remove unused connection event handlers
    Removed commented-out connection event handlers.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       </head>
       <body
         cz-shortcut-listen="true"
-        {/*ononline={()=> toast("😎 Your connection is restored!")}
-        onoffline={()=> toast("👽 Your connection is losed")}*/}
       >
         <ReduxStoreProvider>
           <MantineThemeProvider>
